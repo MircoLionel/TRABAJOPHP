3fad296abd6cdd6b69461cfae72e48262e42155f
Mirco Fermani <100171449+MircoLionel@users.noreply.github.com>
2024-05-29 14:32:05 -0300

    compilo!

--- funciones.php
+++ funciones.php
@@ -1,3 +1,25 @@
 <?php 
 include 'conexion.php';
-?>
+
+if (isset($_POST['check1'])) {
+    $nombreEscuela = trim($_POST['nombreEscuela']);
+    $nombre = trim($_POST['nombre']);
+    $apellido = trim( $_POST['apellido']);
+    $dni = trim($_POST['dni']);
+    $fecDeNac = $_POST['fecDeNac'];
+    $grado = trim($_POST['grado']);
+    $turno = trim($_POST['turno']);
+    $fecDeReg= date("Y-m-d");
+    $reg = "INSERT alumno(nombre, apellido, dni, fecDeNac, curso, turno, fecDeReg) VALUES ('$nombre','$apellido','$dni','$fecDeNac','$grado','$turno','$fecDeReg')";
+    $resultado = mysqli_query($conexion, $reg);
+   if($resultado){
+        ?>
+        <h3 class ="ok">exito</h3>
+        <?php
+    } else {
+        ?>
+        <h3 class ="error">error</h3>
+        <?php
+    }      
+}   
+
--- index.php
+++ index.php
@@ -6,63 +6,59 @@
     <title>Registro Rutas</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
+    <?php include 'funciones.php';
+    ?>
 </head>
 <body>
     <div class="container mt-5">
-    <h1>Registro individual de pasajero</h1>
-    <form>
-        <div class="mb-3">
-            <label for="nombreEscuela" class="form-label">Nombre de Escuela</label>
-            <input type="text" class="form-control" id="nombreEscuela" required>
-        </div>
-        <div class="mb-3">
-            <label for="nombre" class="form-label">Nombre</label>
-            <input type="text" class="form-control" id="nombre" required>
-        </div>
-        <div class="mb-3">
-            <label for="apellido" class="form-label">Apellido</label>
-            <input type="text" class="form-control" id="apellido" required>
-        </div>
-        <div class="mb-3">
-            <label for="dni" class="form-label">Nombre de Escuela</label>
-            <input type="number" class="form-control" id="dni" required>
-        </div>
-        <div class="mb-3">
-            <label for="fecNac" class="form-label">Fecha de nacimiento</label>
-            <input type="date" class="form-control" id="fecNac" required>
-        </div>
-        <div class="mb-3">
-            <label for="anio" class="form-label">Grado</label>
-            <input type="number" class="form-control" id="grado" required>
-        </div>
-        <div class="mb-3">
-            <label for="curso" class="form-label">Seccion</label>
-            <input type="text" class="form-control" id="seccion" required>
-        </div>
-        <div class="mb-3">
-            <label for="turno" class="form-label">Turno</label>
-            <input type="text" class="form-control" id="turno" required>
-        </div>
+        <h1>Registro individual de pasajero</h1>
+        <form method="post">
             <div class="mb-3">
-                <p>Nombre docente</p>
-                <select class="form-select form-select-lg"name="Docente"id="docente">Docente a cargo
-                    <option value="1">Roxana</option>
-                    <option value="2">Marilu</option>
-                    <option value="3">Valeria</option>
-                    <option value="4">Cintia</option>
-                    <option value="5">Norma</option>
-                </select required>
+                <label for="nombreEscuela" class="form-label">Nombre de Escuela</label>
+                <input type="text" class="form-control" name="nombreEscuela" required>
             </div>
+            <div class="mb-3">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" required>
+            </div>
+            <div class="mb-3">
+                <label for="apellido" class="form-label">Apellido</label>
+                <input type="text" class="form-control" name="apellido" required>
+            </div>
+            <div class="mb-3">
+                <label for="dni" class="form-label">Documento nacional de identidad</label>
+                <input type="number" class="form-control" name="dni" required>
+            </div>
+            <div class="mb-3">
+                <label for="fecDeNac" class="form-label">Fecha de nacimiento</label>
+                <input type="date" class="form-control" name="fecDeNac" required>
+            </div>
+            <div class="mb-3">
+                <label for="grado" class="form-label">Grado</label>
+                <input type="number" class="form-control" name="grado" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="turno" class="form-label">Turno</label>
+                <input type="text" class="form-control" name="turno" required>
+            </div>
+                <div class="mb-3">
+                    <p>Nombre docente</p>
+                    <select class="form-select form-select-lg"name="Docente" name="docente">Docente a cargo
+                        <option value="1">Roxana</option>
+                        <option value="2">Marilu</option>
+                        <option value="3">Valeria</option>
+                        <option value="4">Cintia</option>
+                        <option value="5">Norma</option>
+                    </select required>
+                </div>
            
-        
+            
         </div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+        <button type="submit"method="post" name="check1"class="btn btn-primary">Enviar</button>
     </form>
     
-    <?php
-        include (funciones.php);
-    ?>
+   
 
 </div>
 
